ae94190ec5cdf09a2fc1298a020f8a4f003f8c78
Di-ua <di.ua@mail.ru>
2021-02-04 18:24:18 +0200

    changed styles and changed js

--- src/js/background.js
+++ src/js/background.js
@@ -12,22 +12,37 @@ const refs = {
     start: document.querySelector('button[data-action="start"]'),
     stop: document.querySelector('button[data-action="stop"]'),
 }
+// refs.body.dataset.action = false;
+
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
   let intervalId = null;
 
-  const changeColor = () => {
-      const color = randomIntegerFromInterval(0, color.length -1);
-      refs.body.style.backgroundColor = colors[color];
+  refs.body.addEventListener('click', event => {
+    if (event.target === refs.start) {
+        onStartHandler();
+    }
+    if (event.target === refs.stop) {
+        onStopHandler();
+    }
+});
+
+  // refs.body.dataset.action = true;
+  const changeRandomColor = () => {
+      const colorBg = randomIntegerFromInterval(0, colors.length -1);
+      document.body.style.backgroundColor = colors[colorBg];
+      console.log(changeRandomColor);
   }
-  const startHandler = () => {
+  const onStartHandler = () => {
       refs.start.disabled = true;
-      intervalId = setInterval(changeColor, 1000);
+      refs.stop.disabled = false;
+      intervalId = setInterval(changeRandomColor, 1000);
   }
-  const stopHandler = () => {
+  const onStopHandler = () => {
       refs.start.disabled = false;
       refs.stop.disabled = true;
+      // refs.body.dataset.action = false;
       clearInterval(intervalId);
   }
